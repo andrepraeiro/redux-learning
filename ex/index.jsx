af2f58b6d35bef92eed8969f1a9e3b44617ec598
Andre Praeiro <andrepreiro@gmail.com>
2018-06-08 00:15:56 -0400

    finishing class #65 - integrating react-redux

--- ex/index.jsx
+++ ex/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 
+
 import Field from './field';
 
 const reducers = combineReducers({
